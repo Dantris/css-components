6a325c49986df679e1dc8ba5cb1e039bc88666fb
Adrian Dever <ad.dever@yahoo.com>
2025-04-25 22:44:15 +0200

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+switch ( 2 + 2 === 4 ) {
+    case false:
+      console.log( "False." );
+    case true:
+      console.log( "True." );
+  }
+
+switch ( 4 + 4 === 8 ) {
+    case false:
+        console.log( "False." );
+    case true:
+        console.log("True.");
+}
